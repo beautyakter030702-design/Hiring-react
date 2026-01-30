c78789e01477574f0b8e0c8a2b01e7901d6b860b
beautyhasan34 <hasanbeauty57@gmail.com>
2026-01-30 06:47:15 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Hiring from "./component/Hiring";
+
+function App() {
+  return <Hiring />;
+}
+
+export default App;
---
+++ src/component/Hiring.jsx
@@ -0,0 +1,29 @@
+import "./Hiring.css";
+
+export default function Hiring() {
+    return (
+        <div className="hiring-page">
+            <div className="hiring-card">
+
+                <h1 className="title">We Are Hiring!</h1>
+                <p className="subtitle">Join Our Team</p>
+
+                <h2 className="position">Receptionist (Full Time)</h2>
+
+                <ul className="list">
+                    <li>Female candidates only</li>
+                    <li>Good communication skills</li>
+                    <li>Basic computer knowledge</li>
+                    <li>Ability to work with students and guardians</li>
+                    <li>Time management and multitasking skills</li>
+                </ul>
+
+                <div className="apply-box">
+                    <p>Send your CV to:</p>
+                    <strong>beautyakter@gmail.com</strong>
+                </div>
+
+            </div>
+        </div>
+    );
+}
