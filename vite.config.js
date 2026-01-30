b85c19739d8aa4ed3da8e92a29de04557e176f1c
beautyhasan34 <hasanbeauty57@gmail.com>
2026-01-30 07:21:26 -0800

    fix 404 github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: 
+    [tailwindcss(),
+    react()],
   base: '/Hiring-react/',   // 🔴 এটা খুব জরুরি
 })
